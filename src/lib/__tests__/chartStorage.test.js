64ce9ffb25d09ed32359b9158a90d7107508dd16
Will Richards <richawil@oregonstate.edu>
2024-03-12 10:33:28 -0700

    Added testing modules to package.json, started working on chartStorage unit test

---
+++ src/lib/__tests__/chartStorage.test.js
@@ -0,0 +1,13 @@
+require("@testing-library/jest-dom");
+
+const chartStorage = require("../chartStorage");
+const domTesting = require("@testing-library/dom");
+const userEvent = require("@testing-library/user-event").default;
+
+
+/**
+ * Generates some random example chart that we can use to load and save from
+ */
+function generateChart(){
+
+}
