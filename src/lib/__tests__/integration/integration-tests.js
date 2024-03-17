274ca795d215d2098da59c53ef81717b0d3da082
Rooney Ferguson <ferguroo@oregonstate.edu>
2024-03-17 16:31:15 -0700

    Add test for missing chart data

--- src/lib/__tests__/integration/integration-tests.js
+++ src/lib/__tests__/integration/integration-tests.js
@@ -1,6 +1,6 @@
 /**
-* @jest-environment jsdom
-*/
+ * @jest-environment jsdom
+ */
 
 require("@testing-library/jest-dom");
 const domTesting = require("@testing-library/dom");
@@ -9,60 +9,61 @@ const GenerateChartImg = require(`${__dirname}/../../../lib/generateChartImg`);
 
 const fs = require("fs");
 const { request } = require("http");
-require('whatwg-fetch');
+require("whatwg-fetch");
 
+function initDOMFromFiles(htmlPath, jsPath) {
+    const html = fs.readFileSync(htmlPath, "utf8");
+    document.open();
+    document.write(html);
+    document.close();
 
-function initDOMFromFiles(htmlPath, jsPath){
-   const html = fs.readFileSync(htmlPath, 'utf8')
-   document.open();
-   document.write(html);
-   document.close();
-
-   jest.isolateModules(() =>{
-       require(jsPath);
-   });
+    jest.isolateModules(() => {
+        require(jsPath);
+    });
 }
 
 test("Adding values in the chart builder works correctly", async function () {
-   const user = userEvent.setup();
+    const user = userEvent.setup();
 
-   initDOMFromFiles(
-       `${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`
-   );
+    initDOMFromFiles(
+        `${__dirname}/../../../line/line.html`,
+        `${__dirname}/../../../line/line.js`
+    );
 
-   xInputs = domTesting.getAllByLabelText(document, "X");
-   yInputs = domTesting.getAllByLabelText(document, "Y");
-   const addButton = domTesting.getByText(document, "+");
+    xInputs = domTesting.getAllByLabelText(document, "X");
+    yInputs = domTesting.getAllByLabelText(document, "Y");
+    const addButton = domTesting.getByText(document, "+");
 
-   await userEvent.type(xInputs[0], "1");
-   await userEvent.type(yInputs[0], "2");
-   await userEvent.click(addButton);
-   
-   xInputs = domTesting.getAllByLabelText(document, "X");
-   yInputs = domTesting.getAllByLabelText(document, "Y");
+    await userEvent.type(xInputs[0], "1");
+    await userEvent.type(yInputs[0], "2");
+    await userEvent.click(addButton);
 
-   await userEvent.type(xInputs[1], "3");
-   await userEvent.type(yInputs[1], "4");
-   await userEvent.click(addButton);
+    xInputs = domTesting.getAllByLabelText(document, "X");
+    yInputs = domTesting.getAllByLabelText(document, "Y");
 
-   xInputs = domTesting.getAllByLabelText(document, "X");
-   yInputs = domTesting.getAllByLabelText(document, "Y");
+    await userEvent.type(xInputs[1], "3");
+    await userEvent.type(yInputs[1], "4");
+    await userEvent.click(addButton);
 
-   expect(xInputs[0].value).toBe("1");
-   expect(yInputs[0].value).toBe("2");
-   expect(xInputs[1].value).toBe("3");
-   expect(yInputs[1].value).toBe("4");
-   expect(xInputs[2].value).toBe("");
-   expect(yInputs[2].value).toBe("");
+    xInputs = domTesting.getAllByLabelText(document, "X");
+    yInputs = domTesting.getAllByLabelText(document, "Y");
+
+    expect(xInputs[0].value).toBe("1");
+    expect(yInputs[0].value).toBe("2");
+    expect(xInputs[1].value).toBe("3");
+    expect(yInputs[1].value).toBe("4");
+    expect(xInputs[2].value).toBe("");
+    expect(yInputs[2].value).toBe("");
 });
 
 test("Clearing chart data works correctly", async function () {
     const user = userEvent.setup();
- 
+
     initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`
+        `${__dirname}/../../../line/line.html`,
+        `${__dirname}/../../../line/line.js`
     );
- 
+
     xInputs = domTesting.getAllByLabelText(document, "X");
     yInputs = domTesting.getAllByLabelText(document, "Y");
     const xLabel = domTesting.getByLabelText(document, "X label");
@@ -72,27 +73,27 @@ test("Clearing chart data works correctly", async function () {
     const colorPicker = domTesting.getByLabelText(document, "Chart color");
     const clearButton = domTesting.getByText(document, "Clear chart data");
     const originalColor = colorPicker.value;
- 
+
     await userEvent.type(xInputs[0], "1");
     await userEvent.type(yInputs[0], "2");
     await userEvent.click(addButton);
-    
+
     xInputs = domTesting.getAllByLabelText(document, "X");
     yInputs = domTesting.getAllByLabelText(document, "Y");
- 
+
     await userEvent.type(xInputs[1], "3");
     await userEvent.type(yInputs[1], "4");
 
     await userEvent.type(xLabel, "X");
     await userEvent.type(yLabel, "Y");
     await userEvent.type(chartTitle, "Title");
-    colorPicker.value = '#ff0000';
+    colorPicker.value = "#ff0000";
 
     await userEvent.click(clearButton);
 
     xInputs = domTesting.getAllByLabelText(document, "X");
     yInputs = domTesting.getAllByLabelText(document, "Y");
-    
+
     expect(xInputs[0].value).toBe("");
     expect(yInputs[0].value).toBe("");
     expect(xInputs.length).toBe(1);
@@ -101,9 +102,18 @@ test("Clearing chart data works correctly", async function () {
     expect(xLabel.value).toBe("");
     expect(chartTitle.value).toBe("");
     expect(colorPicker).toHaveValue(originalColor);
- });
+});
 
- /*
+test("Alerts displayed for missing chart data", async function () {
+    const user = userEvent.setup();
+
+    initDOMFromFiles(
+        `${__dirname}/../../../line/line.html`,
+        `${__dirname}/../../../line/line.js`
+    );
+});
+
+/*
  test("Data correctly sent to chart generation function", async function () {
     const user = userEvent.setup();
  
