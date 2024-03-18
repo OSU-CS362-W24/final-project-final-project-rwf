66aa86313bc08b386ee6759a0df096e056dbeaa9
Rooney Ferguson <ferguroo@oregonstate.edu>
2024-03-17 22:14:11 -0700

    Add stubbing to the generateChartImg function in data generation passing test

--- src/lib/__tests__/integration/integration-tests.js
+++ src/lib/__tests__/integration/integration-tests.js
@@ -215,14 +215,14 @@ test("Alerts displayed for missing chart axes", async function () {
     spy.mockRestore();
 });
 
-/*
- test("Data correctly sent to chart generation function", async function () {
+test("Data correctly sent to chart generation function", async function () {
     const user = userEvent.setup();
- 
+
     initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`
+        `${__dirname}/../../../line/line.html`,
+        `${__dirname}/../../../line/line.js`
     );
- 
+
     xInputs = domTesting.getAllByLabelText(document, "X");
     yInputs = domTesting.getAllByLabelText(document, "Y");
     const xLabel = domTesting.getByLabelText(document, "X label");
@@ -231,27 +231,40 @@ test("Alerts displayed for missing chart axes", async function () {
     const addButton = domTesting.getByText(document, "+");
     const colorPicker = domTesting.getByLabelText(document, "Chart color");
     const generateButton = domTesting.getByText(document, "Generate chart");
-    const generateChartImgSpy = jest.spyOn(GenerateChartImg, "generateChartImg");
- 
+    const generateChartImgSpy = jest.spyOn(
+        GenerateChartImg,
+        "generateChartImg"
+    );
+    spy.mockImplementation(function () {});
+
     await userEvent.type(xInputs[0], "1");
     await userEvent.type(yInputs[0], "2");
     await userEvent.click(addButton);
-    
+
     xInputs = domTesting.getAllByLabelText(document, "X");
     yInputs = domTesting.getAllByLabelText(document, "Y");
- 
+
     await userEvent.type(xInputs[1], "3");
     await userEvent.type(yInputs[1], "4");
 
     await userEvent.type(xLabel, "X");
     await userEvent.type(yLabel, "Y");
     await userEvent.type(chartTitle, "Title");
-    colorPicker.value = '#ff0000';
+    colorPicker.value = "#ff0000";
 
     await userEvent.click(generateButton);
 
-    expect(generateChartImgSpy).toHaveBeenCalledWith('line', [[1, 2], [3, 4]], 'X', 'Y', 'Title', '#ff0000');
+    expect(generateChartImgSpy).toHaveBeenCalledWith(
+        "line",
+        [
+            [1, 2],
+            [3, 4],
+        ],
+        "X",
+        "Y",
+        "Title",
+        "#ff0000"
+    );
 
     generateChartImgSpy.mockRestore();
- });
-*/
+});
