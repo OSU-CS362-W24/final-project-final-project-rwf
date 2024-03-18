65a86ae9114d34e751f64226c0cafcd4da93a23c
Rooney Ferguson <ferguroo@oregonstate.edu>
2024-03-18 16:28:00 -0700

    Formatting

--- src/lib/__tests__/integration/integration-tests.js
+++ src/lib/__tests__/integration/integration-tests.js
@@ -30,10 +30,7 @@ beforeEach(function () {
 test("Adding values in the chart builder works correctly", async function () {
     const user = userEvent.setup();
 
-    initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`,
-        `${__dirname}/../../../line/line.js`
-    );
+    initDOMFromFiles(`${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`);
 
     let xInputs = domTesting.getAllByLabelText(document, "X");
     let yInputs = domTesting.getAllByLabelText(document, "Y");
@@ -64,10 +61,7 @@ test("Adding values in the chart builder works correctly", async function () {
 test("Clearing chart data works correctly", async function () {
     const user = userEvent.setup();
 
-    initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`,
-        `${__dirname}/../../../line/line.js`
-    );
+    initDOMFromFiles(`${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`);
 
     let xInputs = domTesting.getAllByLabelText(document, "X");
     let yInputs = domTesting.getAllByLabelText(document, "Y");
@@ -112,10 +106,7 @@ test("Clearing chart data works correctly", async function () {
 test("Alerts displayed for missing chart data", async function () {
     // Setup for each test
     const user = userEvent.setup();
-    initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`,
-        `${__dirname}/../../../line/line.js`
-    );
+    initDOMFromFiles(`${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`);
 
     // Add references to dom elements for later use
     const xLabel = domTesting.getByLabelText(document, "X label");
@@ -156,10 +147,7 @@ test("Alerts displayed for missing chart data", async function () {
 test("Alerts displayed for missing chart axes", async function () {
     // Setup for each test
     const user = userEvent.setup();
-    initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`,
-        `${__dirname}/../../../line/line.js`
-    );
+    initDOMFromFiles(`${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`);
 
     // Add references to dom elements for later use
     const xLabel = domTesting.getByLabelText(document, "X label");
@@ -201,9 +189,7 @@ test("Alerts displayed for missing chart axes", async function () {
 
     // Assertions
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy.mock.calls[0][0]).toBe(
-        "Error: Must specify a label for both X and Y!"
-    );
+    expect(spy.mock.calls[0][0]).toBe("Error: Must specify a label for both X and Y!");
     expect(xLabel.value).toBe("");
     expect(yLabel.value).toBe("");
     expect(xInputs.length).toBe(4);
@@ -233,10 +219,7 @@ test("Data correctly sent to chart generation function", async function () {
     });
 
     // Init files
-    initDOMFromFiles(
-        `${__dirname}/../../../line/line.html`,
-        `${__dirname}/../../../line/line.js`
-    );
+    initDOMFromFiles(`${__dirname}/../../../line/line.html`, `${__dirname}/../../../line/line.js`);
 
     // Set variables to access DOM elements later
     let xInputs = domTesting.getAllByLabelText(document, "X");
@@ -280,30 +263,16 @@ test("Data correctly sent to chart generation function", async function () {
 
     // Assertions
     expect(generateChartImgSpy.mock.calls[0][0]).toBe("line");
-    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({
-        x: "4",
-        y: "6",
-    });
-    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({
-        x: "1",
-        y: "6",
-    });
-    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({
-        x: "9",
-        y: "4",
-    });
-    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({
-        x: "2",
-        y: "8",
-    });
+    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({ x: "4", y: "6" });
+    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({ x: "1", y: "6" });
+    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({ x: "9", y: "4" });
+    expect(generateChartImgSpy.mock.calls[0][1]).toContainEqual({ x: "2", y: "8" });
     expect(generateChartImgSpy.mock.calls[0][1].length).toBe(4);
     expect(generateChartImgSpy.mock.calls[0][2]).toBe("X");
     expect(generateChartImgSpy.mock.calls[0][3]).toBe("Y");
     expect(generateChartImgSpy.mock.calls[0][4]).toBe("Title");
     expect(generateChartImgSpy.mock.calls[0][5]).toBe("#ff0000");
-    expect(generateChartImgSpy.mock.results[0].value).toBe(
-        "http://placekitten.com/480/480"
-    );
+    expect(generateChartImgSpy.mock.results[0].value).toBe("http://placekitten.com/480/480");
 
     generateChartImgSpy.mockRestore();
 });
